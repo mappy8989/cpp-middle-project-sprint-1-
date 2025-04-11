3f90456445037cedc8ede4bfda7d99531de66d2c
IgorF <kleef@mail.ru>
2025-04-11 21:16:22 +0300

    Clang format

--- include/crypto_guard_ctx.h
+++ include/crypto_guard_ctx.h
@@ -1,28 +1,35 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 namespace CryptoGuard {
 
 class CryptoGuardCtx {
 public:
-    CryptoGuardCtx() {}
-    ~CryptoGuardCtx() {}
+  CryptoGuardCtx();
+  ~CryptoGuardCtx();
 
-    CryptoGuardCtx(const CryptoGuardCtx &) = delete;
-    CryptoGuardCtx &operator=(const CryptoGuardCtx &) = delete;
+  CryptoGuardCtx(const CryptoGuardCtx &) = delete;
+  CryptoGuardCtx &operator=(const CryptoGuardCtx &) = delete;
 
-    CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
-    CryptoGuardCtx &operator=(CryptoGuardCtx &&) noexcept = default;
+  CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
+  CryptoGuardCtx &operator=(CryptoGuardCtx &&) noexcept = default;
 
-    // API
-    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    std::string CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+  // API
+  void EncryptFile(std::iostream &inStream, std::iostream &outStream,
+                   std::string_view password);
+  void DecryptFile(std::iostream &inStream, std::iostream &outStream,
+                   std::string_view password);
+  std::string CalculateChecksum(std::iostream &inStream) {
+    return "NOT_IMPLEMENTED";
+  }
 
 private:
-    class Impl;
-    Impl *pImpl_;
+  struct Impl;
+  std::unique_ptr<Impl> pImpl_;
 };
 
-}  // namespace CryptoGuard
+// struct CryptoGuardCtx::Impl;
+
+} // namespace CryptoGuard

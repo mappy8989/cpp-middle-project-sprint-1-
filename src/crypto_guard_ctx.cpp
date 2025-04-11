80cd2e46ca9a3a75fe161ba44dd8a99ccee512e7
IgorF <kleef@mail.ru>
2025-04-11 21:16:54 +0300

    CryptoGuardCtx PImpl added

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,31 @@
+#include "crypto_guard_ctx.h"
+#include <iostream>
 
 namespace CryptoGuard {
 
-}  // namespace CryptoGuard
+struct CryptoGuardCtx::Impl {
+  std::string CalculateChecksum(std::iostream &inStream) {
+    return "NOT_IMPLEMENTED";
+  }
+
+  void EncryptFile(std::iostream &inStream, std::iostream &outStream,
+                   std::string_view password) {}
+  void DecryptFile(std::iostream &inStream, std::iostream &outStream,
+                   std::string_view password) {}
+};
+
+CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<Impl>()) {}
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream,
+                                 std::iostream &outStream,
+                                 std::string_view password) {
+  pImpl_->EncryptFile(inStream, outStream, password);
+}
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream,
+                                 std::iostream &outStream,
+                                 std::string_view password) {
+  pImpl_->DecryptFile(inStream, outStream, password);
+}
+
+} // namespace CryptoGuard

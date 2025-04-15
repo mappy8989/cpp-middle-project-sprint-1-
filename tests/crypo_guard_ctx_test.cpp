4e1e09a7b03513884b43596927338bb6c92b62e9
IgorF <kleef@mail.ru>
2025-04-15 21:24:42 +0300

    Fix tests

--- tests/crypo_guard_ctx_test.cpp
+++ tests/crypo_guard_ctx_test.cpp
@@ -1,111 +1,117 @@
 #include "crypto_guard_ctx.h"
+#include <cstdint>
 #include <gtest/gtest.h>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 
-TEST(crypto_guard, encrypt_1) {
-    std::stringstream input("Simple string!");
-    std::stringstream out;
-    CryptoGuard::CryptoGuardCtx guard;
+TEST(crypto_guard, encrypt_string) {
+  std::stringstream input("Simple string!");
+  std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
 
-    ASSERT_NO_THROW(guard.EncryptFile(input, out, "1234"));
+  ASSERT_NO_THROW(guard.EncryptFile(input, out, "1234"));
 }
 
-TEST(crypto_guard, encrypt_2) {
-    std::stringstream input("Simple string!");
-    std::stringstream out;
-    CryptoGuard::CryptoGuardCtx guard;
+TEST(crypto_guard, encrypt_check_size) {
+  std::stringstream input("Simple string!");
+  std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
 
-    guard.EncryptFile(input, out, "1234");
-    std::string line;
-    std::getline(out, line);
-    EXPECT_TRUE(line.size() > 0);
+  guard.EncryptFile(input, out, "1234");
+  std::string line;
+  std::getline(out, line);
+  EXPECT_TRUE(line.size() > 0);
 }
 
-TEST(crypto_guard, encrypt_3) {
-    std::stringstream input("test");
-    std::stringstream out;
-    CryptoGuard::CryptoGuardCtx guard;
+TEST(crypto_guard, encrypt_fail_stream) {
+  std::stringstream input("test");
+  std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
 
-    input.setstate(std::ios::failbit);
+  input.setstate(std::ios::failbit);
 
-    ASSERT_THROW(guard.EncryptFile(input, out, "1234"), std::runtime_error);
+  ASSERT_THROW(guard.EncryptFile(input, out, "1234"), std::runtime_error);
 }
 
-TEST(crypto_guard, decrypt_1) {
-    const std::string test_str = "0123456789a0123456789b0123456789c0123456789d0123456789e0123456789f";
+TEST(crypto_guard, decrypt_short_buf) {
+  const int BYTE_BUF_SIZE = 7;
+  const char byte_buf_in[BYTE_BUF_SIZE] = {1, 2, 3, 4, 5, 0xff, 0xba};
 
-    CryptoGuard::CryptoGuardCtx guard;
-    std::stringstream input(test_str);
-    std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
+  std::stringstream input(byte_buf_in);
+  std::stringstream out;
 
-    guard.EncryptFile(input, out, "1234");
+  guard.EncryptFile(input, out, "1234");
 
-    std::stringstream plain_text;
-    guard.DecryptFile(out, plain_text, "1234");
+  std::stringstream plain_text;
+  guard.DecryptFile(out, plain_text, "1234");
 
-    // as we have padding in the output buffer, we need only a string with
-    // terminating NULL at the end
-    std::string plain_string = plain_text.rdbuf()->str().substr(0, strlen(plain_text.rdbuf()->str().data()));
-
-    EXPECT_EQ(test_str, plain_string);
+  std::string plain_string = plain_text.str();
+  EXPECT_EQ(0, memcmp(byte_buf_in, plain_string.data(), BYTE_BUF_SIZE));
 }
 
-TEST(crypto_guard, decrypt_2) {
-    const std::string test_str = "012345678901234567890123456789012345678901234567890123456789";
+TEST(crypto_guard, decrypt_diff_passwords) {
+  const int BYTE_BUF_SIZE = 7;
+  const char byte_buf_in[BYTE_BUF_SIZE] = {1, 2, 3, 4, 5, 0xff, 0xba};
 
-    CryptoGuard::CryptoGuardCtx guard;
-    std::stringstream input(test_str);
-    std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
+  std::stringstream input(byte_buf_in);
+  std::stringstream out;
 
-    guard.EncryptFile(input, out, "1234");
+  guard.EncryptFile(input, out, "1234");
 
-    std::stringstream plain_text;
-    guard.DecryptFile(out, plain_text, "5678");
-    std::string plain_string = plain_text.rdbuf()->str().substr(0, strlen(plain_text.rdbuf()->str().data()));
-    EXPECT_NE(test_str, plain_string);
+  std::stringstream plain_text;
+  guard.DecryptFile(out, plain_text, "5678");
+
+  std::string plain_string = plain_text.str();
+  EXPECT_NE(0, memcmp(byte_buf_in, plain_string.data(), BYTE_BUF_SIZE));
 }
 
-TEST(crypto_guard, decrypt_3) {
-    const std::string test_str = "Hello, world!";
+TEST(crypto_guard, decrypt_long_buf) {
+  const int BYTE_BUF_SIZE = 1000;
+  char byte_buf_in[BYTE_BUF_SIZE] = {};
 
-    CryptoGuard::CryptoGuardCtx guard;
-    std::stringstream input(test_str);
-    std::stringstream out;
+  uint8_t temp = 0;
+  for (int i = 0; i < BYTE_BUF_SIZE; i++) {
+    byte_buf_in[i] = temp++;
+  }
 
-    guard.EncryptFile(input, out, "1234");
+  CryptoGuard::CryptoGuardCtx guard;
+  std::stringstream input(byte_buf_in);
+  std::stringstream out;
 
-    std::stringstream plain_text;
-    guard.DecryptFile(out, plain_text, "1234");
+  guard.EncryptFile(input, out, "1234");
 
-    // as we have padding in the output buffer, we need only a string with
-    // terminating NULL at the end
-    std::string plain_string = plain_text.rdbuf()->str().substr(0, strlen(plain_text.rdbuf()->str().data()));
+  std::stringstream plain_text;
+  guard.DecryptFile(out, plain_text, "1234");
 
-    EXPECT_NE(test_str.substr(0, test_str.size() / 2), plain_string);
+  std::string plain_string = plain_text.str();
+  EXPECT_NE(0, memcmp(byte_buf_in, plain_string.data(), BYTE_BUF_SIZE));
 }
 
 TEST(crypto_guard, hash_1) {
-    CryptoGuard::CryptoGuardCtx guard;
-    std::stringstream input("123456789");
-    std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
+  std::stringstream input("123456789");
+  std::stringstream out;
 
-    const std::string correct_hash_str = "15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225";
+  const std::string correct_hash_str =
+      "15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225";
 
-    std::string hash = guard.CalculateChecksum(input);
+  std::string hash = guard.CalculateChecksum(input);
 
-    EXPECT_EQ(correct_hash_str, hash);
+  EXPECT_EQ(correct_hash_str, hash);
 }
 
 TEST(crypto_guard, hash_2) {
-    CryptoGuard::CryptoGuardCtx guard;
-    std::stringstream input("1 1");
-    std::stringstream out;
+  CryptoGuard::CryptoGuardCtx guard;
+  std::stringstream input("1 1");
+  std::stringstream out;
 
-    const std::string correct_hash_str = "020a7c91e30725bb191818987340dec6040aff93923840de685e1e1d7b3d071a";
+  const std::string correct_hash_str =
+      "020a7c91e30725bb191818987340dec6040aff93923840de685e1e1d7b3d071a";
 
-    std::string hash = guard.CalculateChecksum(input);
+  std::string hash = guard.CalculateChecksum(input);
 
-    EXPECT_EQ(correct_hash_str, hash);
+  EXPECT_EQ(correct_hash_str, hash);
 }
